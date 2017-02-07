d5c1e502992578e271ff29c9b83d68d1daa22d5d
byanofsky <byanofsky@me.com>
2017-02-07 13:30:45 -0500

    Create flask testing

---
+++ flask_tests.py
@@ -0,0 +1,30 @@
+import os
+import final_project
+import unittest
+import tempfile
+
+
+class final_projectTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, final_project.app.config['DATABASE'] = tempfile.mkstemp()
+        final_project.app.config['TESTING'] = True
+        self.app = final_project.app.test_client()
+        with final_project.app.app_context():
+            # final_project.init_db()
+            pass
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(final_project.app.config['DATABASE'])
+
+    def test_routing(self):
+        rv = self.app.get('/')
+        assert b'All Restaurants' in rv.data
+        rv = self.app.get('/restaurants/')
+        assert b'All Restaurants' in rv.data
+        rv = self.app.get('/restaurant/1/')
+        assert b'Edit Restaurant Name' in rv.data
+
+if __name__ == '__main__':
+    unittest.main()
