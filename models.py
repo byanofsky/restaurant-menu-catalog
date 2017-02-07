92465ee0793391703d1c69002305120aaef5202c
byanofsky <byanofsky@me.com>
2017-02-07 16:55:05 -0500

    Add MenuItems model

--- models.py
+++ models.py
@@ -1,8 +1,20 @@
-from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy import Column, Integer, String, Sequence, ForeignKey
 from database import Base
+from sqlalchemy.orm import relationship
 
 
 class Restaurant(Base):
     __tablename__ = 'restaurants'
     id = Column(Integer, Sequence('restaurant_id_seq'), primary_key=True)
     name = Column(String, nullable=False)
+
+
+class MenuItems(Base):
+    __tablename__ = 'menu-items'
+    id = Column(Integer, Sequence('menu_item_id_seq'), primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    price = Column(String)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+
+    restaurant = relationship(Restaurant)
